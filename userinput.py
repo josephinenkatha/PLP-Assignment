a59b0f2da903c407c2c6e35a05823a6ba064455a
josephinenkatha <josenkathathura@gmail.com>
2024-04-01 15:56:02 +0300

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -2,4 +2,3 @@ name = input('Enter name: ')
 age = input('Enter age: ')
 location = input('Enter location: ')
 print('Hello '+ name + ', you are ' + age + ' years old, and you stay at ' + location)
-jo
